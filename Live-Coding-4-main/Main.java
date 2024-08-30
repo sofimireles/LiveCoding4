f8d5f51d64158e7a2508a0fa2384c9cdfbf15ff3
Sofi Mireles <sofimirelesr@gmail.com>
2024-08-30 16:35:58 -0600

    Add files via upload

---
+++ Live-Coding-4-main/Main.java
@@ -0,0 +1,10 @@
+package com.FizzBuzz;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// creo un objeto de la clase FizzBuzz y llamo a la funcion imprimir.
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		fizzbuzz.imprimir();
+	}
+}
